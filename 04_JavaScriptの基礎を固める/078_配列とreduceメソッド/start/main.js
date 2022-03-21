4ad81a56b84b2f69b90b5f4d1fd83da15e040643
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2022-03-21 18:00:19 +0900

    reruce関数

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,16 @@
-const arry = [1,2,3,4,5];
+// const arry = [1, 2, 3, 4, 5];
+
+// arry.reduce(function (accu, curr) {
+//   console.log(accu, curr);
+//   return accu + curr;
+// });
+
+const str = "animation";
+const strArray = str.split("");
+console.log(strArray);
+
+const result = strArray.reduce((accu, curr) => {
+  return `${accu}<${curr}>`;
+}, "");
+
+console.log(result);

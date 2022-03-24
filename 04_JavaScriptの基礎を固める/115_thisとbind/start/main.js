89c15386de76e010e17a037b393df0bb1d170353
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2022-03-25 07:41:02 +0900

    thisとクラスおさらい

--- 04_JavaScriptの基礎を固める/115_thisとbind/start/main.js
+++ 04_JavaScriptの基礎を固める/115_thisとbind/start/main.js
@@ -1,13 +1,14 @@
 const obj = {
-    first_name: 'Mafia',
-    last_name: 'Code',
-    printFullName: function () {
+  first_name: "Mafia",
+  last_name: "Code",
+  printFullName: function () {
+    console.log(this);
+    window.setTimeout(
+      function () {
         console.log(this);
-
-        window.setTimeout(function () {
-            console.log(this);
-        });
-    }
-}
+      }.bind(this)
+    );
+  },
+};
 
 obj.printFullName();

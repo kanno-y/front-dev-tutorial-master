1bafaeb078f0af8d5a6c29f909c8a27a968581a5
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2022-04-20 06:41:49 +0900

    モバイル用メニュー作成

---
+++ 05_JavaScriptを実践で使う/050_モバイル用メニューを作成しよう/start/main.js
@@ -0,0 +1,29 @@
+class MobileMenu {
+  constructor() {
+    this.DOM = {}; // DOM初期化
+    this.DOM.btn = document.querySelector(".mobile-menu__btn");
+    this.DOM.container = document.querySelector("#gloval-container");
+    this.eventType = this._getEventType();
+    this._addEvent();
+  }
+
+  _getEventType() {
+    const isTouchCapable =
+      "ontouchstart" in window ||
+      (window.DocumentTouch && document instanceof window.DocumentTouch) ||
+      navigator.maxTouchPoints > 0 ||
+      window.navigator.msMaxTouchPoints > 0;
+
+    return isTouchCapable ? "touchstart" : "click";
+  }
+
+  _toggle() {
+    this.DOM.container.classList.toggle("menu-open");
+  }
+
+  _addEvent() {
+    this.DOM.btn.addEventListener("click", this._toggle.bind(this)); // イベントリスナー登録
+  }
+}
+
+new MobileMenu(); // MobileMenuクラスをインスタンス化

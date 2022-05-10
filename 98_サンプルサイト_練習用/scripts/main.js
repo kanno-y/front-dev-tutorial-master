3e27d2ea3e48e6be9e5a37b9be4b0543176ebce3
ykanno0521 <54009743+ykanno0521@users.noreply.github.com>
2022-05-10 18:05:49 +0900

    javascript整理１

--- 98_サンプルサイト_練習用/scripts/main.js
+++ 98_サンプルサイト_練習用/scripts/main.js
@@ -1,33 +1,67 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const hero = new HeroSlider(".swiper-container");
-  hero.start();
+  const main = new Main();
+});
 
-  const cb = function (el, inview) {
+class Main {
+  constructor() {
+    this.header = document.querySelector(".header");
+    this._observers = [];
+    // this._scrollInit();
+    this._init();
+  }
+
+  _init() {
+    new MobileMenu();
+    this.hero = new HeroSlider(".swiper-container");
+    this._scrollInit();
+  }
+
+  _inviewAnimation(el, inview) {
+    if (inview) {
+      el.classList.add("inview");
+    } else {
+      el.classList.remove("inview");
+    }
+  }
+
+  _navAnimation(el, inview) {
+    if (inview) {
+      this.header.classList.remove("triggered");
+    } else {
+      this.header.classList.add("triggered");
+    }
+  }
+
+  _textAnimation(el, inview) {
     if (inview) {
       const ta = new TweenTextAnimation(el);
       ta.animate();
     }
-  };
-
-  const so = new ScrollObserver(".tween-animate-title", cb);
-
-  const _inviewAnimation = (el, inview);
-  if (inview) {
-    el.classList.add("inview");
-  } else {
-    el.classList.remove("inview");
   }
-  const so2 = new ScrollObserver(".cover-slide", _inviewAnimation);
 
-  const header = document.querySelector(".header");
-  const _navAnimation = (el, inview);
-  if (inview) {
-    header.classList.remove("triggered");
-  } else {
-    header.classList.add("triggered");
+  _toggleSlideAnimation(el, inview) {
+    if (inview) {
+      this.hero.start();
+    } else {
+      this.hero.stop();
+    }
   }
-  const so3 = new ScrollObserver(".nav-trigger", _navAnimation, {
-    once: false,
-  });
-});
-const mobile = new MobileMenu();
+  _scrollInit() {
+    this._observers.push(
+      new ScrollObserver(".nav-trigger", this._navAnimation.bind(this), {
+        once: false,
+      })
+    );
+    this._observers.push(
+      new ScrollObserver(".cover-slide", this._inviewAnimation)
+    );
+    new ScrollObserver(".tween-animate-title", this._textAnimation);
+    new ScrollObserver(
+      ".swiper-container",
+      this._toggleSlideAnimation.bind(this),
+      {
+        once: false,
+      }
+    );
+  }
+}
